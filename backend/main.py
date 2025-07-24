190d6bcd67db633e5b8891afb1c9d7b55e331a2f
Will White <willwhite94@outlook.com>
2025-07-24 13:26:46 +1000

    Fixed IP in CORS settings

--- backend/main.py
+++ backend/main.py
@@ -60,7 +60,7 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=["http://170.64.205.252:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
